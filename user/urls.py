9c5157ab337851f8d1ea0f33084fdf894ca4be46
GordPark <gord23p@gmail.com>
2025-08-18 23:36:15 +0900

    feat(auth): add social login with Google SMTP

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,7 @@ app_name = 'user'
 urlpatterns = [
     path('', views.user, name='user'),
     path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('send_verification_code/', views.send_verification_code, name='send_verification_code'),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from .utils import send_verification_email, generate_verification_code
 
 # Create your views here.
 def user(request):
@@ -6,3 +9,23 @@ def user(request):
 
 def login(request):
     return render(request, 'user/login.html')
+
+def register(request):
+    return render(request,'user/register.html' )
+
+def send_verification_code(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        email = data.get('email')
+        
+        if not email:
+            return JsonResponse({'success': False, 'error': '이메일을 입력하세요.'})
+
+        verification_code = generate_verification_code()
+        request.session['email_verification_code'] = verification_code
+        request.session['email'] = email
+        send_verification_email(email, verification_code)
+        
+        return JsonResponse({'success': True})
+
+    return JsonResponse({'success': False, 'error': '유효하지 않은 요청입니다.'})
