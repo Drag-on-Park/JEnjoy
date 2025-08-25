9659e2b8b75451c944a6cc49765d1effa5612bb6
GordPark <gord23p@gmail.com>
2025-08-25 15:42:14 +0900

    feat: Commit for local integration

--- config/settings.py
+++ config/settings.py
@@ -99,11 +99,11 @@ DATABASES = {
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv.("DB_NAME"),
-#         'USER': os.getenv.("DB_USER"),
-#         'PASSWORD': os.getenv.("DB_PASSWORD"),
-#         'HOST': os.getenv.("DB_HOST"),    
-#         'PORT': os.getenv.("DB_PORT"),        # 컨테이너 내부 포트
+#         'NAME': os.getenv("DB_NAME"),
+#         'USER': os.getenv("DB_USER"),
+#         'PASSWORD': os.getenv("DB_PASSWORD"),
+#         'HOST': os.getenv("DB_HOST"),    
+#         'PORT': os.getenv("DB_PORT"),        # 컨테이너 내부 포트
 #     }
 # }
 
